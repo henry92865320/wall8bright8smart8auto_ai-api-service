5dab1a3c85d953ef96872d10070d618512f14e23
henry92865320 <h92865320@gmail.com>
2025-04-07 01:52:03 +0900

    Update development log at 2025-04-07 01:52:02

--- src/protect7zeta7ocean_log.py
+++ src/protect7zeta7ocean_log.py
@@ -76,3 +76,26 @@ class Observer:
     def notify(self):
         for observer in self._observers:
             observer.update()
+
+# Update at 2025-04-07 01:52:02
+# Added unit tests
+# Fixed some bugs
+# Improved performance
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
