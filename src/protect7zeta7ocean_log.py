d38348ef607ed0e7e52f7020393817e710c10925
henry92865320 <h92865320@gmail.com>
2025-04-08 04:14:04 +0900

    Update development log at 2025-04-08 04:14:03

--- src/protect7zeta7ocean_log.py
+++ src/protect7zeta7ocean_log.py
@@ -99,3 +99,12 @@ class LinkedList:
         while current.next:
             current = current.next
         current.next = new_node
+
+# Update at 2025-04-08 04:14:03
+# Refactored the code
+def count_words(text):
+    words = text.split()
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    return word_count
