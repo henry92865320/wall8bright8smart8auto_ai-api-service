141c1fb4b7786a1fa42a19f802f8c28b2a7d4faf
henry92865320 <h92865320@gmail.com>
2025-05-03 08:07:07 +0900

    Update development log at 2025-05-03 08:07:05

--- src/protect7zeta7ocean_log.py
+++ src/protect7zeta7ocean_log.py
@@ -564,3 +564,14 @@ def reverse_string(s):
 def is_palindrome(s):
     s = s.lower().replace(" ", "")
     return s == s[::-1]
+
+# Update at 2025-05-03 08:07:05
+# Added documentation
+import hashlib
+
+def hash_password(password):
+    salt = "random_salt"
+    return hashlib.sha256((password + salt).encode()).hexdigest()
+
+def verify_password(password, hashed):
+    return hash_password(password) == hashed
