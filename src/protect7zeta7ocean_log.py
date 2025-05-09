d6083a30b732b0e8760dbea417e77f4e6d5fbacd
henry92865320 <h92865320@gmail.com>
2025-05-10 01:31:13 +0900

    Update development log at 2025-05-10 01:31:12

--- src/protect7zeta7ocean_log.py
+++ src/protect7zeta7ocean_log.py
@@ -665,3 +665,19 @@ def reverse_string(s):
 def is_palindrome(s):
     s = s.lower().replace(" ", "")
     return s == s[::-1]
+
+# Update at 2025-05-10 01:31:12
+# Refactored the code
+# Improved performance
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
