bed6a64da3bb8140a87d33b2cfda9fd88647da82
henry92865320 <h92865320@gmail.com>
2025-05-11 03:32:08 +0900

    Update development log at 2025-05-11 03:32:07

--- src/protect7zeta7ocean_log.py
+++ src/protect7zeta7ocean_log.py
@@ -681,3 +681,20 @@ def binary_search(arr, target):
         else:
             right = mid - 1
     return -1
+
+# Update at 2025-05-11 03:32:07
+# Fixed some bugs
+# Added some random functionality
+
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
