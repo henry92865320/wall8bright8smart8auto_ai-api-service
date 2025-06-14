5096f8be06b45c2987ca51af9a872a2690e906c5
henry92865320 <h92865320@gmail.com>
2025-06-14 15:38:14 +0900

    Update development log at 2025-06-14 15:38:12

--- src/protect7zeta7ocean_log.py
+++ src/protect7zeta7ocean_log.py
@@ -1261,3 +1261,17 @@ def create_table(conn):
         conn.commit()
     except sqlite3.Error as e:
         print(e)
+
+# Update at 2025-06-14 15:38:12
+# This is a random comment
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
